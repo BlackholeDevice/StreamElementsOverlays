cc722cb084da1d92dd99381fdfaf644b78cd4da3
BlackholeDevice <60747021+BlackholeDevice@users.noreply.github.com>
2024-09-27 17:17:53 -0500

    Add new socket and style configurations to kill feed
    Added configurations for socket modes, host, and endpoint settings. Enhanced style options including font customization and improved formatting for kill feed display. Integrated a new debug client for testing and refined kill message parsing logic.

--- kill-feed/v1/overlay.js
+++ kill-feed/v1/overlay.js
@@ -1,35 +1,86 @@
 let config = {};
-const defaultKill = '{attacker} {icon} {victim}';
+const socketModes = {
+    StreamerbotClient,
+    DebugClient: class {
+        on(channel, callback) {
+            const damageTypes = [ 'Bullet', 'VehicleDestruction', 'Suicide', 'Crash', 'unknown'];
+            const names = ['Kendall','Saul','Selena','Rylee','Ashtyn','Mohammed','Bianca','Lee','Hailie','Hanna','Ishaan','Avery','Trenton','Coby','Katelyn','Hassan','Audrey','Braydon','Juliana','Emanuel','Porter','Corbin','Gillian','Hayden'];
+            const using = ['Laser', 'Ballistic', 'Missile']
+            const random = array => array[Math.floor(Math.random() * array.length)];
+            this.interval = setInterval(() => callback({data: {
+                    "event": "kill-log",
+                    "victim": random(names),
+                    "attacker": random(names),
+                    "damageType": random(damageTypes),
+                    "using": random(using)
+                }}), (config.fadeTime * 0.55) * 1000);
+        }
+    }
+};
+
 
 function checkEvent(event) {
     return !!event.detail;
 }
 
-function getDamageIndicator(damageType) {
-    return damageType;
+function firstMatch(mapping, query) {
+    return Object.entries(mapping).filter(([_name, regex]) => regex.test(query)).map(([name]) => name)[0];
 }
 
-function getVictimName(victim) {
-    console.log('victim', victim);
+function getName(person) {
     const mapping = {
-        'Nine Tails': /Criminal/i,
+        'Outlaw': /Criminal/i,
+        'Nine Tails': /NineTails/i,
         'Security': /Guard/i,
         'Civilian': /civilian/i
     };
 
-    const [match] = Object.entries(mapping).filter(([_name, regex]) => regex.test(victim)).map(([name]) => name)
-    return match || victim;
+    return firstMatch(mapping, person) || person;
+}
+
+function getKillSubtype({using, damageType}) {
+    const mapping = {
+        'ballistic': /ballistic/i,
+        'laser': /laser/i,
+    }
+    const hasSubtype = ['VehicleDestruction', 'Bullet'];
+
+    if(!hasSubtype.includes(damageType)) {
+        return;
+    }
+
+    return firstMatch(mapping, using);
+}
+
+function getKillType(data) {
+    const {damageType} = data;
+    const mapping = {
+        'Bullet': 'fps',
+        'VehicleDestruction': 'ship',
+        'Crash': 'crash',
+        'Suicide': 'backspace'
+    }
+
+    return getKillSubtype(data) || mapping[damageType] || damageType
+}
+
+function getIcon(icon) {
+    if(!icon) {
+        return '';
+    }
+
+    return `<img src="${icon}" height="${config.fontSize}" width="${config.fontSize}">`;
 }
 
 function parseKillMessage(data) {
-    const text = config.ballisticText || defaultKill;
-    data.icon = config.ballisticIcon;
+    const killType = getKillType(data);
+    const text = config[`${killType}Text`] || config.defaultText;
+    data.icon = getIcon(config[`${killType}Icon`] || config.defaultIcon);
     return Object.entries(data).reduce((res, [k,v]) => res.replace(`{${k}}`, v), text);
 }
 
 function generateElement(data) {
-    const {attacker, victim, damageType} = data;
-    return $(`<tr class="fade"><td>${attacker}</td><td>${getDamageIndicator(damageType)}</td><td>${getVictimName(victim)}</td></tr>`);
+    return $(`<span class="row fade">${parseKillMessage(data)}</span>`);
 }
 
 const eventHandlers = {
@@ -37,8 +88,7 @@ const eventHandlers = {
         const $element = generateElement(data);
         $('#main-container').prepend($element);
         const fadeTime = config.fadeTime * 1000;
-        setTimeout(() => $element.addClass('fade'), fadeTime);
-        setTimeout(() => $element.remove(), fadeTime + 1500);
+        setTimeout(() => $element.remove(), fadeTime + 1000);
     }
 };
 
@@ -47,9 +97,10 @@ window.addEventListener("onWidgetLoad", async (event) => {
         return;
     }
     config = event.detail.fieldData;
-    const client = new StreamerbotClient({
+    const client = new socketModes[config.socketMode]({
         host: config.socketHost,
         port: config.socketPort,
+        endpoint: config.endpoint,
         subscribe: 'General.Custom'
     });
     client.on('General.Custom',  ({data: {event, ...data}}) => {
