77791bef3665d26c5329a99413d0ff099e8363d0
BlackholeDevice <60747021+BlackholeDevice@users.noreply.github.com>
2024-10-21 23:58:11 -0500

    Rebuild overlay help docs as angular application.

---
+++ src/help/content-url/content-url.component.ts
@@ -0,0 +1,26 @@
+import {Component, inject, input} from '@angular/core';
+import {MatAnchor} from '@angular/material/button';
+import {ContentRetrieverService} from './content-retriever.service';
+import {NavigatorService} from '../../navigator.service';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-content-url',
+  templateUrl: './content-url.component.html',
+  styleUrl: './content-url.component.scss',
+  standalone: true,
+  imports: [
+    MatAnchor,
+    MatSnackBarModule
+  ],
+  providers: [ContentRetrieverService, NavigatorService],
+})
+export class ContentUrlComponent {
+  private service = inject(ContentRetrieverService);
+  public url = input('');
+
+  public copyContent(): boolean {
+    this.service.fetchAndCopy(this.url());
+    return false;
+  }
+}
